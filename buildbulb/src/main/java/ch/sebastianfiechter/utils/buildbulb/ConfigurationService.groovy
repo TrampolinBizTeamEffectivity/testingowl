b1418558b473029affe972fea5a644992564e0df
rbqq <registration@sebastianfiechter.ch>
2013-10-19 00:21:32 +0200

    created target distro

--- buildbulb/src/main/java/ch/sebastianfiechter/utils/buildbulb/ConfigurationService.groovy
+++ buildbulb/src/main/java/ch/sebastianfiechter/utils/buildbulb/ConfigurationService.groovy
@@ -13,8 +13,8 @@ class ConfigurationService {
 	}
 	
 	def readConfig() {
-		def pathToApp = new File(".").getCanonicalPath()
-		config = new XmlSlurper().parse(pathToApp+"\\configuration.xml");
+		//def pathToApp = new File(".").getCanonicalPath()
+		config = new XmlSlurper().parse("configuration.xml");
 	}
 	
 }
