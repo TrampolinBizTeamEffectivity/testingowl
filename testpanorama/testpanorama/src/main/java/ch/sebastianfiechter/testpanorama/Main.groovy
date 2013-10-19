4f8f01e1fc7d8805fcabf45b0b0239aca5d007de
rbqq <registration@sebastianfiechter.ch>
2013-10-19 15:53:51 +0200

    added java-screen-recorder sources

--- testpanorama/testpanorama/src/main/java/ch/sebastianfiechter/testpanorama/Main.groovy
+++ testpanorama/testpanorama/src/main/java/ch/sebastianfiechter/testpanorama/Main.groovy
@@ -6,8 +6,15 @@ import org.springframework.stereotype.Component
 import org.springframework.context.ApplicationContext
 import org.springframework.context.support.ClassPathXmlApplicationContext
 
+import com.wet.wired.jsr.recorder.*
+
 @Component
 class Main {
-
+	
+	static main(args) {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/applicationContext.xml");
+		JRecorder.main(new String[0])
+		
+	}
 
 }
