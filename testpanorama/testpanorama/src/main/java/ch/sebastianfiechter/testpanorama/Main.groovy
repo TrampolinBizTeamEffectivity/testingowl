bec9274e608c475b349189fb53eead27a11a4049
rbqq <registration@sebastianfiechter.ch>
2013-10-19 15:35:09 +0200

    initial commit

---
+++ testpanorama/testpanorama/src/main/java/ch/sebastianfiechter/testpanorama/Main.groovy
@@ -0,0 +1,13 @@
+package ch.sebastianfiechter.testpanorama
+
+
+import org.springframework.beans.factory.annotation.*
+import org.springframework.stereotype.Component
+import org.springframework.context.ApplicationContext
+import org.springframework.context.support.ClassPathXmlApplicationContext
+
+@Component
+class Main {
+
+
+}
